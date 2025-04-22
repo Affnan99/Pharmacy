5caeea36525e7d75aea5aa198dc30f456c8de8bb
Affnan99 <86743028+Affnan99@users.noreply.github.com>
2025-04-22 20:09:33 +0530

    Add files via upload

---
+++ storage/framework/views/d4833167a0454eaa5138867a668c59452762e55f.php
@@ -0,0 +1,2 @@
+
+<?php echo $__env->make('Layout.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\New Folder\Diploma In Ict-Hardy\Second Semester\React Projects\Asky File\Pharmacy\Laravel-Pharmacy-Management-System-master\resources\views/welcome.blade.php ENDPATH**/ ?>
